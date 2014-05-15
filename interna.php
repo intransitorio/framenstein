6f82cbf9158e01123450ef2be1dfecf42cb8b061
Ricardo Fadini <rk.fadini@gmail.com>
2014-05-15 11:45:25 -0300

    Adição de .htaccess padrão e plugins performático.
    Adição do arquivo .htaccess com compactação, cache e url’s amigáveis.
    Adição de plugins em php que diminui a quantidade de requisições.

---
+++ interna.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="pt-br"> <!--<![endif]-->
+    <head>
+        <?php include("_include/head.php"); ?>
+    <body>
+        <!--[if lte IE 9]><p class="chromeframe">Você está usando um navegador desatualizado. <a href="http://browsehappy.com/">Atualize seu navegador</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">instale o Google Chrome</a> para melhor experiência com este site.</p> <![endif]-->
+		
+        <div id="wrapper">
+            <?php include("_include/header.php"); ?>
+            <div id="content">
+                
+            </div>
+        </div>
+        <?php include("_include/footer.php"); ?>
+        
+        <?php include("_include/scripts.php"); ?>
+
+    </body>
+</html>
