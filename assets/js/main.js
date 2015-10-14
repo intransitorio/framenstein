25ff08aff604cce1be435a12ff4fae99e47e1e88
João Felipe Alves <joao@woodoo.com.br>
2015-10-14 02:37:13 -0300

    Framenstein 2.5.1 Ammend

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,57 @@
 $(window).on("load", function() {
-    // Carrossel fundo home
-        $('.carrossel-fundo .carrossel').each(function(){
-            $(this).carouFredSel({
-                auto         : {
-                    play     : true,
-                    timeoutDuration: 3500
-                },
-                swipe        : true,
-                width        : '100%',
-                height       : $(this).data('height'),
-                responsive   : true,
-                items        : {
-                    height   : 'variable',
-                    visible  : 1
-                },
-                scroll       : {
-                    duration : 250,
-                    items    : 1,
-                    fx       : 'crossfade'
-                }
-            });
-        })
-    // Smooth Scroll
-        $('a[href*=#]:not([href=#])').on( 'click', function($event) {
-            $click_nav = true;
-            var $this = $(this);
-            $event.preventDefault();
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-             var target = $(this.hash);
-             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-             if (target.length) {
-                 $('.logo').addClass('scrolling');
-                 $('html,body').animate({
-                   scrollTop: target.offset().top
-                 }, 1000, function() {
-                     $click_nav = false;
-                     $(window).trigger('scroll');
-                     $('.logo').removeClass('scrolling');
-                     //window.location.hash = $this.attr("href");
-                 });
-             }
-            }
-        });    
-    // Padrão Fancybox
-        $(".fancybox").fancybox({
-            padding     : 0,
-            margin      : 0,
-            helpers: {
-                overlay: {
-                  locked: false
-                }
-            }
-        });
+	// Carrossel fundo home
+		$('.carrossel-fundo .carrossel').each(function(){
+			$(this).carouFredSel({
+				auto         : {
+					play     : true,
+					timeoutDuration: 3500
+				},
+				swipe        : true,
+				width        : '100%',
+				height       : $(this).data('height'),
+				responsive   : true,
+				items        : {
+					height   : 'variable',
+					visible  : 1
+				},
+				scroll       : {
+					duration : 250,
+					items    : 1,
+					fx       : 'crossfade'
+				}
+			});
+		})
+	// Smooth Scroll
+		$('a[href*=#]:not([href=#])').on( 'click', function($event) {
+			$click_nav = true;
+			var $this = $(this);
+			$event.preventDefault();
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			 var target = $(this.hash);
+			 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			 if (target.length) {
+				 $('.logo').addClass('scrolling');
+				 $('html,body').animate({
+				   scrollTop: target.offset().top
+				 }, 1000, function() {
+					 $click_nav = false;
+					 $(window).trigger('scroll');
+					 $('.logo').removeClass('scrolling');
+					 //window.location.hash = $this.attr("href");
+				 });
+			 }
+			}
+		});    
+	// Padrão Fancybox
+		$(".fancybox").fancybox({
+			padding     : 0,
+			margin      : 0,
+			helpers: {
+				overlay: {
+				  locked: false
+				}
+			}
+		});
 });
 
 $(window).on("resize", function() {
--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <?php include("include/head.php"); ?>
-    </head>
-    <body class="desktop ou mobile?" onload="$('.loader').removeClass('loading');">
-        <?php include("include/loader.php"); ?>
-        <div class="carrossel-wrapper carrossel-fundo">
-            <div class="carrossel">
-                <img class="item" src="assets/img/bg/home-1.jpg" alt="">
-                <img class="item" src="assets/img/bg/home-2.jpg" alt="">
-                <img class="item" src="assets/img/bg/home-3.jpg" alt="">
-            </div>
-        </div>
-        <div id="wrapper">
-            <?php include("include/header.php"); ?>
-            <div id="content">
-            </div>
-        </div>
-        <?php include("include/footer.php"); ?>        
-        <?php include("include/scripts.php"); ?>
-    </body>
+	<head>
+		<?php include("include/head.php"); ?>
+	</head>
+	<body class="desktop" onload="$('.loader').removeClass('loading');">
+		<?php include("include/loader.php"); ?>
+		<div class="carrossel-wrapper carrossel-fundo">
+			<div class="carrossel">
+				<img class="item" src="assets/img/bg/home-1.jpg" alt="">
+				<img class="item" src="assets/img/bg/home-2.jpg" alt="">
+				<img class="item" src="assets/img/bg/home-3.jpg" alt="">
+			</div>
+		</div>
+		<div id="wrapper">
+			<?php include("include/header.php"); ?>
+			<div id="content">
+			</div>
+		</div>
+		<?php include("include/footer.php"); ?>        
+		<?php include("include/scripts.php"); ?>
+	</body>
 </html>
