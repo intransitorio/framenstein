a7b60ded9db1ae6ce60b8f83093b7ed72d9e55a3
João Felipe Alves <joao@woodoo.com.br>
2016-05-31 17:39:35 -0300

    Framenstein 3.0 -Gulp e outras alterações legais

--- include/scripts.php
+++ include/scripts.php
@@ -1,3 +1,4 @@
-<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-2.1.3.min.js"><\/script>')</script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
 <script src="assets/plugins/load/carouFredSel/chosen/Fancybox/index.js"></script>
 <script src="assets/js/main.js"></script>
